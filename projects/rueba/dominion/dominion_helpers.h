80d804a8fabeee268113d54c6c257f4dcea4cbe2
Alex Rueb <rueba@oregonstate.edu>
2019-04-18 22:33:56 -0500

    refactored cardEffect code into separate functions and introduced bugs

--- projects/rueba/dominion/dominion_helpers.h
+++ projects/rueba/dominion/dominion_helpers.h
@@ -5,11 +5,18 @@
 
 int drawCard(int player, struct gameState *state);
 int updateCoins(int player, struct gameState *state, int bonus);
-int discardCard(int handPos, int currentPlayer, struct gameState *state, 
+int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
-int cardEffect(int card, int choice1, int choice2, int choice3, 
+
+int smithyEffect(int currentPlayer, struct gameState *state, int handPos);
+int adventurerEffect(int currentPlayer,struct gameState *state, int handPos, int temphand[], int z);
+int councilRoomEffect(int currentPlayer, struct gameState *state, int handPos);
+int greatHallEffect(int currentPlayer, struct gameState *state, int handPos);
+int stewardEffect(int currentPlayer, struct gameState *state, int handPos, int choice1, int choice2, int choice3);
+
+int cardEffect(int card, int choice1, int choice2, int choice3,
 	       struct gameState *state, int handPos, int *bonus);
 
 #endif
